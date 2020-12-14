424226c8c2ac3dcbb98d4566347c835093bf9fc2
K.S.KIM <nullable@kakao.com>
2020-12-14 13:45:17 +0900

    test: Iterable과 Spread 연산 활용 테스트 추가

--- test/grammar/Generator.test.ts
+++ test/grammar/Generator.test.ts
@@ -1,9 +1,9 @@
 import { numberRangeGenerator, gapNumberRangeGenerator } from "../../src/grammar/Generator";
 
 describe("반복기 학습 테스트", () => {
-  test("순차 값 반복기를 생성한다.", () => {
+  test("순차 값 반복기를 생성하여 for...of문을 수행한다.", () => {
     // given
-    const iterable = numberRangeGenerator(1, 6);
+    const iterable: Iterable<number> = numberRangeGenerator(1, 6);
 
     // when
     const actual: Array<number> = [];
@@ -15,9 +15,9 @@ describe("반복기 학습 테스트", () => {
     expect(actual).toEqual(expect.arrayContaining([1, 2, 3, 4, 5]));
   });
 
-  test("반복기를 생성한다.", () => {
+  test("반복기를 생성기를 생성하여 for...of문을 수행한다.", () => {
     // given
-    const iterable = gapNumberRangeGenerator(1, 6, 2);
+    const iterable: Iterable<number> = gapNumberRangeGenerator(1, 6, 2);
 
     // when
     const actual: Array<number> = [];
@@ -27,5 +27,15 @@ describe("반복기 학습 테스트", () => {
 
     // then
     expect(actual).toEqual(expect.arrayContaining([1, 3, 5]));
+  });
+  test("반복기 생성기와 Spread 연산을 이용하여 배열을 생성한다.", () => {
+    // given
+    const iterable: Iterable<number> = gapNumberRangeGenerator(1, 10, 2);
+
+    // when
+    const actual: Array<number> = [...iterable];
+
+    // then
+    expect(actual).toEqual(expect.arrayContaining([1, 3, 5, 7, 9]));
   });
 });
