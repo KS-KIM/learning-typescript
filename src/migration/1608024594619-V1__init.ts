6b91cdf0c236a9370af4fc73a1ed4856c4159102
K.S.KIM <nullable@kakao.com>
2020-12-15 18:30:41 +0900

    refactor: db migration 파일 추가

---
+++ src/migration/1608024594619-V1__init.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class V1_init1608024594619 implements MigrationInterface {
+    name = 'V1_init1608024594619'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `firstName` varchar(255) NOT NULL, `lastName` varchar(255) NOT NULL, `age` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("DROP TABLE `user`");
+    }
+
+}
